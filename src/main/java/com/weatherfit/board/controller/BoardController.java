26cec2962f74ac531de0bf306873d44deea39354
Johyunik <whgusdlr13@naver.com>
2023-11-14 12:20:50 +0900

    controller test

---
+++ src/main/java/com/weatherfit/board/controller/BoardController.java
@@ -0,0 +1,11 @@
+package com.weatherfit.board.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+
+    public String test() {
+        return "test";
+    }
+}
