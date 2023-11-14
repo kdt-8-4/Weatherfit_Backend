589332ec92c2ab0369ee21c04fa284cafeb76e63
Johyunik <whgusdlr13@naver.com>
2023-11-14 12:23:05 +0900

    test

--- src/main/java/com/weatherfit/board/controller/BoardController.java
+++ src/main/java/com/weatherfit/board/controller/BoardController.java
@@ -1,10 +1,12 @@
 package com.weatherfit.board.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BoardController {
 
+    @GetMapping("/test")
     public String test() {
         return "test";
     }
