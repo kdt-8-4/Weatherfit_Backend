b673bf7220a2d18f93cbe8a8264ce0dc3efe7dad
HeeSung98 <wlfkrdl2@gmail.com>
2023-11-15 12:54:48 +0900

    인증 인가 제거

--- user/src/main/java/com/weather/controller/WeatherController.java
+++ user/src/main/java/com/weather/controller/WeatherController.java
@@ -1,20 +1,15 @@
 package com.weather.controller;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @Log4j2
 public class WeatherController {
-
     @GetMapping("/hello")
-    public String test(Model model) {
+    public String test() {
         log.info("test!!");
-        return "test";
+        return "hello";
     }
 }
