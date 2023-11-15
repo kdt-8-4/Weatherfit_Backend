0bbb8412676e6918a266127bd23861da60f8ebe0
HeeSung98 <wlfkrdl2@gmail.com>
2023-11-15 10:13:15 +0900

    권한 변경

--- user/src/main/java/com/weather/controller/WeatherController.java
+++ user/src/main/java/com/weather/controller/WeatherController.java
@@ -1,10 +1,12 @@
 package com.weather.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WeatherController {
+    @PreAuthorize("permitAll()")
     @GetMapping("/test")
     public String test() {
         return "test!!";
