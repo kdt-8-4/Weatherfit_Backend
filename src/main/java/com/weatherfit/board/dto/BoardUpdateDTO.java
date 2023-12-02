5986500bb3fb2b52e37e6bbfc67819aebf1359f4
Johyunik <whgusdlr13@naver.com>
2023-12-02 21:57:50 +0900

    수정

--- src/main/java/com/weatherfit/board/dto/BoardUpdateDTO.java
+++ src/main/java/com/weatherfit/board/dto/BoardUpdateDTO.java
@@ -3,6 +3,7 @@ package com.weatherfit.board.dto;
 import com.weatherfit.board.domain.ImageEntity;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
 @Getter
@@ -14,9 +15,6 @@ public class BoardUpdateDTO {
     private List<String> category;
     private List<String> hashTag;
     private List<ImageEntity> images;
-<<<<<<< HEAD
     private String[] deletedImages;
-=======
-       private String[] deletedImages;
->>>>>>> 2ca267785a1aa67187f05e2b056b70f571d417bf
+
 }
